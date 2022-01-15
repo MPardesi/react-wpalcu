810b278df2806d8b505b27ecd91c9acdd4e61d0a
MPardesi <mahuri125@gmail.com>
2022-01-15 11:15:32 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import "./style.css";
 import Home from "./components/Home";
 import Aboutus from "./components/Aboutus";
 import Contact from "./components/Contact";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 export default function App() {
   return (
+    // <div>
+    //   <Home />
+    //   <Contact />
+    //   <Aboutus />
+    // </div>
+
     <div>
-      <Home />
-      <Contact />
-      <Aboutus />
+      <Router>
+         <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/Contact" element={<Contact />} />
+            <Route path="/Aboutus" element={<Aboutus />} />
+          </Routes>
+        </Router>
     </div>
   );
 }
